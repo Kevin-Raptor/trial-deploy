3fa570206b305a524c68e2ec40b4c8d9242e9366
Kevin-Raptor <kevin@raptordesign.in>
2020-04-10 14:03:45 +0530

    Updates

---
+++ precache-manifest.e6dcf25495e5effec8540e76016ed841.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec07232d5f64d163cb26035723d0eaf9",
+    "url": "/trial-deploy/index.html"
+  },
+  {
+    "revision": "08ff4e2c7f4e88f2900c",
+    "url": "/trial-deploy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "827f8763311de38a1ee6",
+    "url": "/trial-deploy/static/js/2.7f3bde12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trial-deploy/static/js/2.7f3bde12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08ff4e2c7f4e88f2900c",
+    "url": "/trial-deploy/static/js/main.a5718bf7.chunk.js"
+  },
+  {
+    "revision": "0df5ca312d2773416cc8",
+    "url": "/trial-deploy/static/js/runtime-main.da7019f9.js"
+  }
+]);
